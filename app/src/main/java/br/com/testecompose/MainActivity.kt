7f293e99302d62c77199e6e615184573408ef806
Cristian Massaneiro <96593642+crismassaneiro@users.noreply.github.com>
2022-10-24 20:47:20 -0300

    Att

--- app/src/main/java/br/com/testecompose/MainActivity.kt
+++ app/src/main/java/br/com/testecompose/MainActivity.kt
@@ -3,12 +3,16 @@ package br.com.testecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.testecompose.ui.theme.TesteComposeTheme
 
@@ -17,18 +21,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
 
-            MessageCard()
+            MessageCard(name = "Cristian")
         }
     }
 
     @Composable
-    fun MessageCard() {
-        Text(text = "Olá, Cristian")
+    fun MessageCard(name: String) {
+        Image(
+            painter = painterResource(id = R.drawable.Itachi),
+            contentDescription = "Imagem do Itachi Uchiha"
+        )
+        Column() {
+            Text(text = "Olá, ${name}")
+            Text(text = "Seja bem vindo ao App!")
+        }
     }
 
     @Preview
     @Composable
-    fun PreviewMessageCard(){
-        MessageCard()
+    fun PreviewMessageCard() {
+        MessageCard(name = "Cristian")
     }
 }
