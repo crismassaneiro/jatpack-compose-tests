244f1aa26cab5699b1c5918753ef212f3a97b828
Cristian Massaneiro <96593642+crismassaneiro@users.noreply.github.com>
2022-10-24 20:58:42 -0300

    Att 1

--- app/src/main/java/br/com/testecompose/MainActivity.kt
+++ app/src/main/java/br/com/testecompose/MainActivity.kt
@@ -4,16 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.testecompose.ui.theme.TesteComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,11 +29,16 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun MessageCard(name: String) {
         Image(
-            painter = painterResource(id = R.drawable.Itachi),
-            contentDescription = "Imagem do Itachi Uchiha"
+            painter = painterResource(id = R.drawable.img),
+            contentDescription = "Imagem do Itachi Uchiha",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
         )
-        Column() {
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
             Text(text = "Olá, ${name}")
+            Spacer(modifier = Modifier.width(4.dp))
             Text(text = "Seja bem vindo ao App!")
         }
     }
